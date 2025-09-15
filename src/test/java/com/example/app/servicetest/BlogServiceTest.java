0e6d6c459960959d30f27859c04e8efb7fe24fe4
johnpastor4356-cmyk <johnpastor4356@gmail.com>
2025-09-15 04:21:51 -0700

    ADDED : Blog Service Unit test

--- src/test/java/com/example/app/servicetest/BlogServiceTest.java
+++ src/test/java/com/example/app/servicetest/BlogServiceTest.java
@@ -1,12 +1,15 @@
 package com.example.app.servicetest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -59,5 +62,81 @@ class BlogServiceTest {
 		assertThat(blogs).isNotNull();
 		assertThat(blogs).hasSize(2);
 		assertThat(blogs.get(0).getTitle()).isEqualTo("Test Blog 1");
+	}
+	
+	@Test
+	@DisplayName("GET BLOG BY ID")
+	public void getBlogByIdTest() {
+		when(blogRepository.findById(2L)).thenReturn(Optional.of(blog2));
+		
+		Optional<Blog> blog = blogService.getBlogById(2L);
+		
+		assertThat(blog).isPresent();
+		assertThat(blog.get().getTitle()).isEqualTo("Test Blog 2");
+		
+	}
+	
+	@Test
+	@DisplayName("ADD BLOG")
+	public void addBlogTest() {
+		
+		Blog newBlog = new Blog();
+		newBlog.setId(3L);
+		newBlog.setTitle("Blog 3");
+		newBlog.setContent("hey, this is from blog 3");
+		newBlog.setAuthor("Ben Hur");
+		newBlog.setDate(Date.valueOf(LocalDate.of(2025, 9, 15)));
+		
+		when(blogRepository.save(newBlog)).thenReturn(newBlog);
+		
+		Blog savedBlog = blogService.addBlog(newBlog);
+		
+		assertThat(savedBlog).isNotNull();
+		assertThat(savedBlog.getTitle()).isEqualTo(newBlog.getTitle());
+		assertThat(savedBlog.getAuthor()).isEqualTo(newBlog.getAuthor());
+		assertThat(savedBlog.getDate())
+					.isEqualTo(Date.valueOf(LocalDate.of(2025, 9, 15)));
+		
+		verify(blogRepository, times(1)).save(newBlog);	
+	}
+	
+	@Test
+	@DisplayName("DELETE BLOG")
+	public void deleteBlogTest() {
+		blogService.deleteBlogById(2L);
+		
+		verify(blogRepository, times(1)).deleteById(2L);
+		
+		when(blogRepository.findById(2L)).thenReturn(Optional.empty());
+		
+		Optional<Blog> deletedNonExistentBlog = blogService.getBlogById(2L);
+		assertThat(deletedNonExistentBlog).isEmpty(); 
+	}
+	
+	@Test
+	@DisplayName("UPDATE BLOG")
+	public void updateBlogTest() {
+		Blog blogToUpdate;
+		
+		when(blogRepository.findById(2L)).thenReturn(Optional.of(blog2));
+
+		blogToUpdate = blogService.getBlogById(2L).get();
+
+		blogToUpdate.setContent(blog2.getContent());
+		blogToUpdate.setDate(blog2.getDate());
+		blogToUpdate.setId(blog2.getId());
+		
+		blogToUpdate.setTitle("Second Blog Updated Title");
+		blogToUpdate.setAuthor("Rango");
+		
+		when(blogRepository.save(blogToUpdate)).thenReturn(blogToUpdate);
+		
+		Blog updatedBlog = blogService.updateBlogById(2L, blogToUpdate);
+		
+		verify(blogRepository, times(1)).save(blogToUpdate);
+		
+		assertThat(updatedBlog).isNotNull();
+		assertThat(updatedBlog.getTitle()).isEqualTo(blogToUpdate.getTitle());
+		assertThat(updatedBlog.getAuthor()).isEqualTo(blogToUpdate.getAuthor());
 	}
 }
